877113925973a8a5b4aaa438ced81e051c097e74
Everton Paixão <everton.paixao16@gmail.com>
2020-11-18 12:18:31 -0300

    chore: inicial estructure of project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { IndexPage } from './modules/checkout/pages';
+
+// STYLES
+import './core/resources/styles/index.scss';
+
+function App() {
+  return (
+    <>
+      <IndexPage />
+    </>
+  );
+}
+
+export default App;
