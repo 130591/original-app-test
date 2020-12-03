17d13c42471ac0c69f283f7001c5032bedf0f1a8
Everton Paixão <everton.paixao16@gmail.com>
2020-12-02 23:01:27 -0300

    refactor: add change for disabled state

--- src/core/resources/widgets/buttons/index.js
+++ src/core/resources/widgets/buttons/index.js
@@ -3,18 +3,18 @@ import React , {  useState  } from 'react';
 export const Button = ({
   children,
   onPress,
-  styled,
   className,
   size,
+  style,
   disabled
 }) => {
   return (
     <button
       className={`c-btn ${className}`}
       style={{ maxWidth: size == 'large' ? '400px' : '200px' }}
-      style={styled}
       onClick={(e) => onPress(e)}
       disabled={disabled}
+      style={style}
     >
       {children}
     </button>
